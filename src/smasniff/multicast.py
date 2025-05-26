64961adb064afe297d25d69c7e5c6b359a44dd84
Jonathan Hahn <jonathan.hahn@t-online.de>
2025-05-26 23:05:48 +0200

    multicast: add recv_async method

--- src/smasniff/multicast.py
+++ src/smasniff/multicast.py
@@ -1,3 +1,4 @@
+import asyncio
 import socket
 import struct
 
@@ -9,19 +10,19 @@ def create_multicast_receiver_socket(ip, port, timeout=None):
 
     if timeout:
         sock.settimeout(timeout)
-    
+
     # allow other processes to bind to the same port
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     try:
-        sock.setsocktop(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     except AttributeError:
         pass # unavailable on system
 
     # bind to all available interfaces
     sock.bind(("", port))
 
-    return MulticastSubscriberSocket(sock, create_multicast_membership_report(ip))    
+    return MulticastSubscriberSocket(sock, create_multicast_membership_report(ip))
 
 
 def create_multicast_membership_report(ip: str) -> bytes:
@@ -30,15 +31,22 @@ def create_multicast_membership_report(ip: str) -> bytes:
 
 
 class MulticastSubscriberSocket:
-  
+
     def __init__(self, sock: socket.socket, membership_report: bytes):
         self._sock = sock
-        self._membership_report = membership_report        
+        self._membership_report = membership_report
 
     def recv(self, buffer_size: int, *args) -> bytes | None:
         """Receive data from the multicast socket."""
-   
+
         return self._sock.recv(buffer_size, *args)
+
+    async def recv_async(self, buffer_size: int) -> bytes | None:
+        """Receive data from the multicast socket."""
+
+        data = await asyncio.get_running_loop().sock_recv(self._sock, buffer_size)
+
+        return data
 
     def subscribe(self):
         """Subscribe to the multicast group."""
