7cabf76fde4e245e0b4997284aa4787ef20798d9
Jonathan Hahn <jon.hahn@posteo.de>
2025-11-01 10:20:09 +0100

    add speedwire client logging information

---
+++ src/smasniff/speedwire_client.py
@@ -0,0 +1,23 @@
+from smasniff.multicast_client import open_socket
+from smasniff.speedwire import decode_telegram
+
+
+SPEEDWIRE_MULTICAST_IP = "239.12.255.254"
+SPEEDWIRE_MULTICAST_PORT = 9522
+RX_TIMEOUT = 4.0 #s
+
+
+def main():
+
+    # open the igmp multicast socket
+    with open_socket(SPEEDWIRE_MULTICAST_IP, SPEEDWIRE_MULTICAST_PORT, RX_TIMEOUT) as s:
+
+        # receive 609 byte frames as long as possible
+        while frame:= s.recv(608):
+
+            # ensure we got 608 bytes, otherwise decoding will fail
+            if len(frame) != 608:
+                continue
+            
+            print(decode_telegram(frame))
+        
